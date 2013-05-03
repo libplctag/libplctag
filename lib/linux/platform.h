b01027bcd7a5fb47af8895a38dffbcf0f53fd237
Kyle Hayes <kyle.hayes@gmail.com>
2013-05-03 10:00:18 -0700

    Fixed file headers.  Had old names and no copyright info etc.  Oops!

--- lib/linux/platform.h
+++ lib/linux/platform.h
@@ -1,9 +1,30 @@
-/*
- * os.h
- *
- *  Created on: Nov 27, 2012
- *      Author: Kyle Hayes
- */
+/***************************************************************************
+ *   Copyright (C) 2012 by Process Control Engineers                       *
+ *   Author Kyle Hayes  kylehayes@processcontrolengineers.com              *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+ /**************************************************************************
+  * CHANGE LOG                                                             *
+  *                                                                        *
+  * 2012-11-27  KRH - Created file from old platform dependent code.       *
+  *                   Genericized thread creation.                         *
+  *                                                                        *
+  **************************************************************************/
 
 #ifndef OS_H_
 #define OS_H_
