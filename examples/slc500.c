3202f2e2b6cda70d50f8252f69c25e325ea3b905
Kyle Hayes <kyle.hayes@gmail.com>
2014-11-20 11:09:39 -0800

    Added test for SLC5/05.  Works for INT and FLOAT.

---
+++ examples/slc500.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "../lib/libplctag.h"
+
+
+#define TAG_PATH "protocol=ab_eip&gateway=10.206.1.26&cpu=SLC&elem_size=2&elem_count=1&name=N7:0&debug=1"
+#define ELEM_COUNT 1
+#define ELEM_SIZE 2
+#define DATA_TIMEOUT 5000
+
+
+int main(int argc, char **argv)
+{
+    plc_tag tag = PLC_TAG_NULL;
+    int rc;
+    int i;
+
+    /* create the tag */
+    tag = plc_tag_create(TAG_PATH);
+
+    /* everything OK? */
+    if(!tag) {
+        fprintf(stderr,"ERROR: Could not create tag!\n");
+
+        return 0;
+    }
+
+    /* let the connect succeed we hope */
+    while(plc_tag_status(tag) == PLCTAG_STATUS_PENDING) {
+    	sleep(1);
+    }
+
+    if(plc_tag_status(tag) != PLCTAG_STATUS_OK) {
+    	fprintf(stderr,"Error setting up tag internal state.\n");
+    	return 0;
+    }
+
+    /* get the data */
+    rc = plc_tag_read(tag, DATA_TIMEOUT);
+
+    if(rc != PLCTAG_STATUS_OK) {
+        fprintf(stderr,"ERROR: Unable to read the data! Got error code %d\n",rc);
+
+        return 0;
+    }
+
+    /* print out the data */
+    for(i=0; i < ELEM_COUNT; i++) {
+        fprintf(stderr,"data[%d]=%d\n",i,plc_tag_get_int16(tag,(i*ELEM_SIZE)));
+    }
+
+    /* now test a write */
+    for(i=0; i < ELEM_COUNT; i++) {
+    	int16_t val = plc_tag_get_int16(tag,(i*ELEM_SIZE));
+
+    	val = val+1;
+
+    	fprintf(stderr,"Setting element %d to %d\n",i,val);
+
+    	plc_tag_set_int16(tag,(i*ELEM_SIZE),val);
+    }
+
+    rc = plc_tag_write(tag, DATA_TIMEOUT);
+
+    if(rc != PLCTAG_STATUS_OK) {
+        fprintf(stderr,"ERROR: Unable to read the data! Got error code %d\n",rc);
+
+        return 0;
+    }
+
+
+    /* get the data again*/
+    rc = plc_tag_read(tag, DATA_TIMEOUT);
+
+    if(rc != PLCTAG_STATUS_OK) {
+        fprintf(stderr,"ERROR: Unable to read the data! Got error code %d\n",rc);
+
+        return 0;
+    }
+
+    /* print out the data */
+    for(i=0; i < ELEM_COUNT; i++) {
+        fprintf(stderr,"data[%d]=%d\n",i,plc_tag_get_int16(tag,(i*ELEM_SIZE)));
+    }
+
+    /* we are done */
+    plc_tag_destroy(tag);
+
+    return 0;
+}
+
+
