e217a55d4305612800098642d366d413e1fdf076
Kyle Hayes <kyle.hayes@gmail.com>
2016-09-17 14:12:43 -0700

    Added fixes for splint warning on read cache setting.
    The read cache ms setting was directly set from the attribs.  This was a possible
    fault because it could be negative in the attribs.  Added checking to make sure that
    negatives were clamped to zero and then a cast to make it uint64_t.

--- src/lib/libplctag_tag.c
+++ src/lib/libplctag_tag.c
@@ -109,7 +109,6 @@ LIB_EXPORT plc_tag plc_tag_create(const char *attrib_str)
             read_cache_ms = 0;
         }
 
-
         tag->read_cache_expire = (uint64_t)0;
         tag->read_cache_ms = (uint64_t)read_cache_ms;
 
