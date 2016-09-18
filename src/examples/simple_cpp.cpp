d9bf9a57061883d5b831fd36a5cf27e7d66f6a67
Kyle Hayes <kyle.hayes@gmail.com>
2016-09-18 16:48:45 -0700

    Changed code to use utils.h routines for portability.

--- src/examples/simple_cpp.cpp
+++ src/examples/simple_cpp.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <unistd.h>
 #include "../lib/libplctag.h"
+#include "utils.h"
 
 
 #define TAG_PATH "protocol=ab_eip&gateway=192.168.1.42&path=1,0&cpu=LGX&elem_size=4&elem_count=10&name=myDINTArray"
@@ -33,20 +33,19 @@ int main(int argc, char **argv)
 
     /* let the connect succeed we hope */
     while(plc_tag_status(tag) == PLCTAG_STATUS_PENDING) {
-    	sleep(1);
+        sleep_ms(100);
     }
 
     if(plc_tag_status(tag) != PLCTAG_STATUS_OK) {
-    	fprintf(stderr,"Error setting up tag internal state.\n");
-    	return 0;
+        fprintf(stderr,"Error setting up tag internal state. Error %s\n", plc_tag_decode_error(plc_tag_status(tag)));
+        return 0;
     }
 
     /* get the data */
     rc = plc_tag_read(tag, DATA_TIMEOUT);
 
     if(rc != PLCTAG_STATUS_OK) {
-        fprintf(stderr,"ERROR: Unable to read the data! Got error code %d\n",rc);
-
+        fprintf(stderr,"ERROR: Unable to read the data! Got error code %d: %s\n",rc, plc_tag_decode_error(rc));
         return 0;
     }
 
@@ -57,20 +56,19 @@ int main(int argc, char **argv)
 
     /* now test a write */
     for(i=0; i < ELEM_COUNT; i++) {
-    	int32_t val = plc_tag_get_int32(tag,(i*ELEM_SIZE));
+        int32_t val = plc_tag_get_int32(tag,(i*ELEM_SIZE));
 
-    	val = val+1;
+        val = val+1;
 
-    	fprintf(stderr,"Setting element %d to %d\n",i,val);
+        fprintf(stderr,"Setting element %d to %d\n",i,val);
 
-    	plc_tag_set_int32(tag,(i*ELEM_SIZE),val);
+        plc_tag_set_int32(tag,(i*ELEM_SIZE),val);
     }
 
     rc = plc_tag_write(tag, DATA_TIMEOUT);
 
     if(rc != PLCTAG_STATUS_OK) {
-        fprintf(stderr,"ERROR: Unable to read the data! Got error code %d\n",rc);
-
+        fprintf(stderr,"ERROR: Unable to read the data! Got error code %d: %s\n",rc, plc_tag_decode_error(rc));
         return 0;
     }
 
@@ -79,8 +77,7 @@ int main(int argc, char **argv)
     rc = plc_tag_read(tag, DATA_TIMEOUT);
 
     if(rc != PLCTAG_STATUS_OK) {
-        fprintf(stderr,"ERROR: Unable to read the data! Got error code %d\n",rc);
-
+        fprintf(stderr,"ERROR: Unable to read the data! Got error code %d: %s\n",rc, plc_tag_decode_error(rc));
         return 0;
     }
 
