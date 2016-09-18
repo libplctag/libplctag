1266c62e3ae95e1a3e367fca02aff0bd8d459945
Kyle Hayes <kyle.hayes@gmail.com>
2016-09-18 16:46:13 -0700

    Separated sleep_ms(), time_ms() code out into shared utils.
    Separation of the sleep_ms() and time_ms() code into shared utils.  These
    are essentially copies of the code in platform.c.

---
+++ src/examples/utils.h
@@ -0,0 +1,51 @@
+/***************************************************************************
+ *   Copyright (C) 2016 by OmanTek                                         *
+ *   Author Kyle Hayes  kylehayes@omantek.com                              *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef __EXAMPLE_UTILS_H__
+#define __EXAMPLE_UTILS_H__
+
+#ifdef _WIN32
+    #include <windows.h>
+    #define strcasecmp _stricmp
+    #define strdup _strdup
+#else
+    #include <unistd.h>
+    #include <strings.h>
+#endif
+
+
+#include <stdint.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int sleep_ms(int ms);
+extern int64_t time_ms(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
+
---
+++ src/examples/utils_posix.c
@@ -0,0 +1,63 @@
+/***************************************************************************
+ *   Copyright (C) 2016 by OmanTek                                         *
+ *   Author Kyle Hayes  kylehayes@omantek.com                              *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+
+#include "utils.h"
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdarg.h>
+#include <string.h>
+#include <sys/time.h>
+
+/*
+ * This file contains useful utilities for the sample programs.
+ */
+
+
+
+int sleep_ms(int ms)
+{
+    struct timeval tv;
+
+    tv.tv_sec = ms/1000;
+    tv.tv_usec = (ms % 1000)*1000;
+
+    return select(0,NULL,NULL,NULL, &tv);
+}
+
+
+/*
+ * time_ms
+ *
+ * Return the current epoch time in milliseconds.
+ */
+int64_t time_ms(void)
+{
+    struct timeval tv;
+
+    gettimeofday(&tv,NULL);
+
+    return  ((int64_t)tv.tv_sec*1000)+ ((int64_t)tv.tv_usec/1000);
+}
+
+
+
+
