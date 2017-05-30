9542e65d6e768c1c48d88b9a060685ed55c4db4c
Kyle Hayes <kyle.hayes@gmail.com>
2017-05-29 21:40:12 -0700

    Changing default run time to 30 seconds.
    One hour is fine for very long tests, but for quick sanity checks, it is too long.

--- src/examples/stress_test.c
+++ src/examples/stress_test.c
@@ -243,7 +243,7 @@ int main(int argc, char **argv)
     pthread_t threads[MAX_THREADS];
     int64_t start_time;
     int64_t end_time;
-    int64_t seconds = 3600;  /* default 1 hour */
+    int64_t seconds = 30;  /* default 1 hour */
     int num_threads = 0;
 
     if(argc>2) {
