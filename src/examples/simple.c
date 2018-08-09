4a6cc0753b40f723d19f59371ccac54a81fb343c
Kyle Hayes <kyle.hayes@gmail.com>
2018-08-08 20:46:07 -0700

    Changed tag URL to point to simulator.

--- src/examples/simple.c
+++ src/examples/simple.c
@@ -24,7 +24,7 @@
 #include "utils.h"
 
 
-#define TAG_PATH "protocol=ab-eip&gateway=10.206.1.39&path=1,5&cpu=micro800&elem_size=4&elem_count=200&name=TestBigArray&debug=4"
+#define TAG_PATH "protocol=ab-eip&gateway=127.0.0.1&path=1,5&cpu=micro800&elem_size=4&elem_count=200&name=TestBigArray&debug=4"
 #define ELEM_COUNT 200
 #define ELEM_SIZE 4
 #define DATA_TIMEOUT 5000
