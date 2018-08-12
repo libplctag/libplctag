6fa0e720c5aa9c6d8672e6e50308d3c671a51c2a
Kyle Hayes <kyle.hayes@gmail.com>
2018-08-12 13:02:46 -0700

    Adding Logix simulator to code base.
    This is a _VERY_ rudimentary simulator.  It only supports two tags that
    are hard coded to DINT types.   The tags are created in code only.   It takes
    no command line arguments for tag types.
    
    It only works with C11.   C99 is not supported.  It only works on Linux (or
    at least it is only tested on Linux).

--- src/tests/lgx_sim/main.c
+++ src/tests/lgx_sim/main.c
@@ -11,13 +11,12 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <netdb.h>
-#include <errno.h>
 #include <pthread.h>
 #include "log.h"
 #include "session.h"
 #include "tags.h"
 
-#define PORT    44818 /* Port to listen on */
+#define PORT    "44818" /* Port to listen on */
 #define BACKLOG     10  /* Passed to listen() */
 
 static int init_socket(int *sock);
@@ -28,49 +27,37 @@ int init_socket(int *sock)
 {
     struct addrinfo hints, *res;
     int reuseaddr = 1; /* True */
-    struct sockaddr_in listen_addr;
+
+    /* Get the address info */
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    if (getaddrinfo(NULL, PORT, &hints, &res) != 0) {
+        log("getaddrinfo() failed!\n");
+        return 1;
+    }
 
     /* Create the socket */
-    *sock = socket (PF_INET, SOCK_STREAM, 0);
+    *sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
     if (*sock == -1) {
         log("socket() failed.\n");
-        return 0;
+        return 1;
     }
 
     /* Enable the socket to reuse the address */
-    if (setsockopt(*sock, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(reuseaddr)) == -1) {
+    if (setsockopt(*sock, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(int)) == -1) {
         log("setsockopt() failed!\n");
-        return 0;
+        return 1;
     }
 
-    /* describe where we want to listen: on any network. */
-    memset(&listen_addr, 0, sizeof(listen_addr));
-    listen_addr.sin_family = AF_INET;
-    listen_addr.sin_port = htons(PORT);
-    listen_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    if (bind(*sock, (struct sockaddr *) &listen_addr, sizeof (listen_addr)) < 0) {
+    /* Bind to the address */
+    if (bind(*sock, res->ai_addr, res->ai_addrlen) == -1) {
         log("bind() failed!\n");
-        log("errno = %d\n", errno);
         return 0;
     }
 
-//    /* Get the address info */
-//    memset(&hints, 0, sizeof hints);
-//    hints.ai_family = AF_INET;
-//    hints.ai_socktype = SOCK_STREAM;
-//    if (getaddrinfo(NULL, PORT, &hints, &res) != 0) {
-//        log("getaddrinfo() failed!\n");
-//        return 1;
-//    }
-//
-//    /* Bind to the address */
-//    if (bind(*sock, res->ai_addr, res->ai_addrlen) == -1) {
-//        log("bind() failed!\n");
-//        return 0;
-//    }
-//
-//    freeaddrinfo(res);
-//
+    freeaddrinfo(res);
+
     /* Listen */
     if (listen(*sock, BACKLOG) == -1) {
         log("listen");
@@ -104,7 +91,7 @@ int main(void)
             log("accept() failed!\n");
         } else {
             log("Got a connection from %s on port %d\n",
-                inet_ntoa(their_addr.sin_addr), htons(their_addr.sin_port));
+                   inet_ntoa(their_addr.sin_addr), htons(their_addr.sin_port));
             /* Make a safe copy of newsock */
             int *safesock = malloc(sizeof(int));
             if (safesock) {
