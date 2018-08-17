e51aee4a7fbb18352234968f127fbd6d2dc6a2aa
Kyle Hayes <kyle.hayes@gmail.com>
2018-08-16 20:09:17 -0700

    Small fixes to get working again.   Moved the vtables into the PLC-specific files.
    This moves the vtable into the PLC specific files.

--- src/protocols/ab/eip_cip.c
+++ src/protocols/ab/eip_cip.c
@@ -105,26 +105,10 @@ int tag_status(ab_tag_p tag)
     if (tag->read_in_progress) {
         return PLCTAG_STATUS_PENDING;
     }
-//        if(tag->connection) {
-//            rc = check_read_status_connected(tag);
-//        } else {
-//            rc = check_read_status_unconnected(tag);
-//        }
-//
-//        return rc;
-//    }
-//
+
     if (tag->write_in_progress) {
         return PLCTAG_STATUS_PENDING;
     }
-//        if(tag->connection) {
-//            rc = check_write_status_connected(tag);
-//        } else {
-//            rc = check_write_status_unconnected(tag);
-//        }
-//
-//        return rc;
-//    }
 
     /* We need to treat the session and connection statuses
      * as async because we might not be the thread creating those
