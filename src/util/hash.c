d4309f9ae183e7c6e96e6dd0631dd673fc8f7288
kyle-github <kyle.hayes@gmail.com>
2018-09-03 14:32:02 -0700

    Added suppression of switch/case fall through warnings.
    The hashing function uses fallthrough intentionally.   However, compilers generally
    see this as a warning condition.   Unfortunately, MSVC++ and GCC/Clang use very different
    ways to suppress the warnings.

--- src/util/hash.c
+++ src/util/hash.c
@@ -62,16 +62,16 @@ For every delta with one or two bit set, and the deltas of all three
   have at least 1/4 probability of changing.
 * If mix() is run forward, every bit of c will change between 1/3 and
   2/3 of the time.  (Well, 22/100 and 78/100 for some 2-bit deltas.)
-mix() was built out of 36 single-cycle latency instructions in a 
+mix() was built out of 36 single-cycle latency instructions in a
   structure that could supported 2x parallelism, like so:
-      a -= b; 
+      a -= b;
       a -= c; x = (c>>13);
       b -= c; a ^= x;
       b -= a; x = (a<<8);
       c -= a; b ^= x;
       c -= b; x = (b>>13);
       ...
-  Unfortunately, superscalar Pentiums and Sparcs can't take advantage 
+  Unfortunately, superscalar Pentiums and Sparcs can't take advantage
   of that parallelism.  They've also turned some of those single-cycle
   latency instructions into multi-cycle latency instructions.  Still,
   this is the fastest good hash I could find.  There were about 2^^68
@@ -79,31 +79,31 @@ mix() was built out of 36 single-cycle latency instructions in a
 --------------------------------------------------------------------
 */
 #define mix(a,b,c) \
-{ \
-  a -= b; a -= c; a ^= (c>>13); \
-  b -= c; b -= a; b ^= (a<<8); \
-  c -= a; c -= b; c ^= (b>>13); \
-  a -= b; a -= c; a ^= (c>>12);  \
-  b -= c; b -= a; b ^= (a<<16); \
-  c -= a; c -= b; c ^= (b>>5); \
-  a -= b; a -= c; a ^= (c>>3);  \
-  b -= c; b -= a; b ^= (a<<10); \
-  c -= a; c -= b; c ^= (b>>15); \
-}
+    { \
+        a -= b; a -= c; a ^= (c>>13); \
+        b -= c; b -= a; b ^= (a<<8); \
+        c -= a; c -= b; c ^= (b>>13); \
+        a -= b; a -= c; a ^= (c>>12);  \
+        b -= c; b -= a; b ^= (a<<16); \
+        c -= a; c -= b; c ^= (b>>5); \
+        a -= b; a -= c; a ^= (c>>3);  \
+        b -= c; b -= a; b ^= (a<<10); \
+        c -= a; c -= b; c ^= (b>>15); \
+    }
 
 /* same, but slower, works on systems that might have 8 byte ub4's */
 #define mix2(a,b,c) \
-{ \
-  a -= b; a -= c; a ^= (c>>13); \
-  b -= c; b -= a; b ^= (a<< 8); \
-  c -= a; c -= b; c ^= ((b&0xffffffff)>>13); \
-  a -= b; a -= c; a ^= ((c&0xffffffff)>>12); \
-  b -= c; b -= a; b = (b ^ (a<<16)) & 0xffffffff; \
-  c -= a; c -= b; c = (c ^ (b>> 5)) & 0xffffffff; \
-  a -= b; a -= c; a = (a ^ (c>> 3)) & 0xffffffff; \
-  b -= c; b -= a; b = (b ^ (a<<10)) & 0xffffffff; \
-  c -= a; c -= b; c = (c ^ (b>>15)) & 0xffffffff; \
-}
+    { \
+        a -= b; a -= c; a ^= (c>>13); \
+        b -= c; b -= a; b ^= (a<< 8); \
+        c -= a; c -= b; c ^= ((b&0xffffffff)>>13); \
+        a -= b; a -= c; a ^= ((c&0xffffffff)>>12); \
+        b -= c; b -= a; b = (b ^ (a<<16)) & 0xffffffff; \
+        c -= a; c -= b; c = (c ^ (b>> 5)) & 0xffffffff; \
+        a -= b; a -= c; a = (a ^ (c>> 3)) & 0xffffffff; \
+        b -= c; b -= a; b = (b ^ (a<<10)) & 0xffffffff; \
+        c -= a; c -= b; c = (c ^ (b>>15)) & 0xffffffff; \
+    }
 
 /*
 --------------------------------------------------------------------
@@ -133,46 +133,74 @@ acceptable.  Do NOT use for cryptographic purposes.
 --------------------------------------------------------------------
 */
 
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable: 4061)
+#endif
+
 uint32_t hash( uint8_t *k, size_t length, uint32_t initval)
 {
-   uint32_t a,b,c,len;
+    uint32_t a,b,c,len;
 
-   /* Set up the internal state */
-   len = length;
-   a = b = 0x9e3779b9;  /* the golden ratio; an arbitrary value */
-   c = initval;           /* the previous hash value */
+    /* Set up the internal state */
+    len = length;
+    a = b = 0x9e3779b9;  /* the golden ratio; an arbitrary value */
+    c = initval;           /* the previous hash value */
 
-   /*---------------------------------------- handle most of the key */
-   while (len >= 12)
-   {
-      a += (k[0] +((ub4)k[1]<<8) +((ub4)k[2]<<16) +((ub4)k[3]<<24));
-      b += (k[4] +((ub4)k[5]<<8) +((ub4)k[6]<<16) +((ub4)k[7]<<24));
-      c += (k[8] +((ub4)k[9]<<8) +((ub4)k[10]<<16)+((ub4)k[11]<<24));
-      mix(a,b,c);
-      k += 12; len -= 12;
-   }
+    /*---------------------------------------- handle most of the key */
+    while (len >= 12) {
+        a += (k[0] +((ub4)k[1]<<8) +((ub4)k[2]<<16) +((ub4)k[3]<<24));
+        b += (k[4] +((ub4)k[5]<<8) +((ub4)k[6]<<16) +((ub4)k[7]<<24));
+        c += (k[8] +((ub4)k[9]<<8) +((ub4)k[10]<<16)+((ub4)k[11]<<24));
+        mix(a,b,c);
+        k += 12;
+        len -= 12;
+    }
 
-   /*------------------------------------- handle the last 11 bytes */
-   c += length;
-   switch(len)              /* all the case statements fall through */
-   {
-   case 11: c+=((ub4)k[10]<<24);
-   case 10: c+=((ub4)k[9]<<16);
-   case 9 : c+=((ub4)k[8]<<8);
-      /* the first byte of c is reserved for the length */
-   case 8 : b+=((ub4)k[7]<<24);
-   case 7 : b+=((ub4)k[6]<<16);
-   case 6 : b+=((ub4)k[5]<<8);
-   case 5 : b+=k[4];
-   case 4 : a+=((ub4)k[3]<<24);
-   case 3 : a+=((ub4)k[2]<<16);
-   case 2 : a+=((ub4)k[1]<<8);
-   case 1 : a+=k[0];
-     /* case 0: nothing left to add */
-   }
-   mix(a,b,c);
-   /*-------------------------------------------- report the result */
-   return c;
+    /*------------------------------------- handle the last 11 bytes */
+    c += length;
+    switch(len) {            /* all the case statements fall through */
+    case 11:
+        c+=((ub4)k[10]<<24);
+        /* Falls through. */
+    case 10:
+        c+=((ub4)k[9]<<16);
+        /* Falls through. */
+    case 9 :
+        c+=((ub4)k[8]<<8);
+        /* the first byte of c is reserved for the length */
+        /* Falls through. */
+    case 8 :
+        b+=((ub4)k[7]<<24);
+        /* Falls through. */
+    case 7 :
+        b+=((ub4)k[6]<<16);
+        /* Falls through. */
+    case 6 :
+        b+=((ub4)k[5]<<8);
+        /* Falls through. */
+    case 5 :
+        b+=k[4];
+        /* Falls through. */
+    case 4 :
+        a+=((ub4)k[3]<<24);
+        /* Falls through. */
+    case 3 :
+        a+=((ub4)k[2]<<16);
+        /* Falls through. */
+    case 2 :
+        a+=((ub4)k[1]<<8);
+        /* Falls through. */
+    case 1 :
+        a+=k[0];
+        /* case 0: nothing left to add */
+        /* Falls through. */
+    }
+    mix(a,b,c);
+    /*-------------------------------------------- report the result */
+    return c;
 }
 
-
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
