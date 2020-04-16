3d17f9b9748e622b0fd2597fa2aa9039c83ab590
Kyle Hayes <kyle.hayes@gmail.com>
2020-04-15 21:06:45 -0700

    oops, forgot test_shutdown.c

---
+++ src/examples/test_shutdown.c
@@ -0,0 +1,88 @@
+/***************************************************************************
+ *   Copyright (C) 2020 by Kyle Hayes                                      *
+ *   Author Kyle Hayes  kyle.hayes@gmail.com                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../lib/libplctag.h"
+#include "utils.h"
+
+#define REQUIRED_VERSION 2,2,0
+
+#define TAG_PATH "protocol=ab-eip&gateway=10.206.1.39&path=1,5&cpu=LGX&elem_size=4&elem_count=1&name=TestDINTArray"
+#define DATA_TIMEOUT 5000
+
+
+void test_tag(void)
+{
+    int32_t tag = 0;
+    int rc = PLCTAG_STATUS_OK;
+
+    /* create the tag */
+    tag = plc_tag_create(TAG_PATH, DATA_TIMEOUT);
+
+    /* everything OK? */
+    if(tag < 0) {
+        fprintf(stderr,"ERROR %s: Could not create tag!\n", plc_tag_decode_error(tag));
+        exit(1);
+    }
+
+    if((rc = plc_tag_status(tag)) != PLCTAG_STATUS_OK) {
+        fprintf(stderr,"Error setting up tag internal state. Error %s\n", plc_tag_decode_error(rc));
+        plc_tag_destroy(tag);
+        exit(1);
+    }
+
+    /* get the data */
+    rc = plc_tag_read(tag, DATA_TIMEOUT);
+    if(rc != PLCTAG_STATUS_OK) {
+        fprintf(stderr,"ERROR: Unable to read the data! Got error code %d: %s\n",rc, plc_tag_decode_error(rc));
+        plc_tag_destroy(tag);
+        exit(1);
+    }
+
+    /* print out the data */
+    fprintf(stderr,"data=%d\n", plc_tag_get_int32(tag,0));
+
+    /* we are done */
+    plc_tag_destroy(tag);
+}
+
+int main()
+{
+    /* check the library version. */
+    if(plc_tag_check_lib_version(REQUIRED_VERSION) != PLCTAG_STATUS_OK) {
+        fprintf(stderr, "Required compatible library version %d.%d.%d not available!", REQUIRED_VERSION);
+        exit(1);
+    }
+
+    /* turn on debugging. */
+    plc_tag_set_debug_level(PLCTAG_DEBUG_DETAIL);
+
+    test_tag();
+
+    plc_tag_shutdown();
+
+    test_tag();
+
+    return 0;
+}
+
+
