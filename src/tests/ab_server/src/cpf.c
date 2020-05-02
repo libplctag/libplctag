ee218c2279a41d8755cd57fdf5f6149945bc2a80
Kyle Hayes <kyle.hayes@gmail.com>
2020-05-02 09:44:45 -0700

    adding AB server code into test directory.

---
+++ src/tests/ab_server/src/cpf.c
@@ -0,0 +1,220 @@
+/***************************************************************************
+ *   Copyright (C) 2020 by Kyle Hayes                                      *
+ *   Author Kyle Hayes  kyle.hayes@gmail.com                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <stdint.h>
+#include "cip.h"
+#include "cpf.h"
+#include "eip.h"
+#include "utils.h"
+
+#define CPF_ITEM_NAI ((uint16_t)0x0000) /* NULL Address Item */
+#define CPF_ITEM_CAI ((uint16_t)0x00A1) /* connected address item */
+#define CPF_ITEM_CDI ((uint16_t)0x00B1) /* connected data item */
+#define CPF_ITEM_UDI ((uint16_t)0x00B2) /* Unconnected data item */
+
+
+typedef struct {
+    uint32_t interface_handle;
+    uint16_t router_timeout;
+    uint16_t item_count;        /* should be 2 for now. */
+    uint16_t item_addr_type;
+    uint16_t item_addr_length;
+    uint16_t item_data_type;
+    uint16_t item_data_length;
+} cpf_uc_header_s;
+
+#define CPF_UCONN_HEADER_SIZE (16)
+
+typedef struct {
+    uint32_t interface_handle;
+    uint16_t router_timeout;
+    uint16_t item_count;        /* should be 2 for now. */
+    uint16_t item_addr_type;
+    uint16_t item_addr_length;
+    uint32_t conn_id;
+    uint16_t item_data_type;
+    uint16_t item_data_length;
+    uint16_t conn_seq;
+} cpf_co_header_s;
+
+#define CPF_CONN_HEADER_SIZE (22)
+
+
+
+slice_s handle_cpf_unconnected(slice_s input, slice_s output, plc_s *plc)
+{
+    slice_s result;
+    cpf_uc_header_s header;
+
+    info("handle_cpf_unconnected(): got packet:");
+    slice_dump(input);
+
+    /* we must have some sort of payload. */
+    if(slice_len(input) <= CPF_UCONN_HEADER_SIZE) {
+        info("Unusable size of unconnected CPF packet!");
+        return slice_make_err(EIP_ERR_BAD_REQUEST);
+    }
+
+    /* unpack the request. */
+    header.interface_handle = slice_get_uint32_le(input, 0);
+    header.router_timeout = slice_get_uint16_le(input, 4);
+    header.item_count = slice_get_uint16_le(input, 6);
+
+    /* sanity check the number of items. */
+    if(header.item_count != (uint16_t)2) {
+        info("Unsupported unconnected CPF packet, expected two items but found %u!", header.item_count);
+        return slice_make_err(EIP_ERR_BAD_REQUEST);
+    }
+
+    header.item_addr_type = slice_get_uint16_le(input, 8);
+    header.item_addr_length = slice_get_uint16_le(input, 10);
+    header.item_data_type = slice_get_uint16_le(input, 12);
+    header.item_data_length = slice_get_uint16_le(input, 14);
+
+    /* sanity check the data. */
+    if(header.item_addr_type != CPF_ITEM_NAI) {
+        info("Expected null address item but found %x!", header.item_addr_type);
+        return slice_make_err(EIP_ERR_BAD_REQUEST);
+    }
+
+    if(header.item_addr_length != 0) {
+        info("Expected zero address item length but found %d bytes!", header.item_addr_length);
+        return slice_make_err(EIP_ERR_BAD_REQUEST);
+    }
+
+    if(header.item_data_type != CPF_ITEM_UDI) {
+        info("Expected unconnected data item but found %x!", header.item_data_type);
+        return slice_make_err(EIP_ERR_BAD_REQUEST);
+    }
+
+    if(header.item_data_length != (slice_len(input) - CPF_UCONN_HEADER_SIZE)) {
+        info("CPF unconnected payload length, %d, does not match passed length, %d!", (slice_len(input) - CPF_UCONN_HEADER_SIZE - 2), header.item_data_length);
+        return slice_make_err(EIP_ERR_BAD_REQUEST);
+    }
+
+    /* dispatch and handle the result. */
+    result = cip_dispatch_request(slice_from_slice(input, (size_t)CPF_UCONN_HEADER_SIZE, (size_t)((uint16_t)slice_len(input) - CPF_UCONN_HEADER_SIZE)),
+                                slice_from_slice(output, (size_t)CPF_UCONN_HEADER_SIZE, (size_t)((uint16_t)slice_len(output) - CPF_UCONN_HEADER_SIZE)),
+                                plc);
+
+    if(!slice_has_err(result)) {
+        /* build outbound header. */
+        slice_set_uint32_le(output, 0, header.interface_handle);
+        slice_set_uint16_le(output, 4, header.router_timeout);
+        slice_set_uint16_le(output, 6, 2); /* two items. */
+        slice_set_uint16_le(output, 8, CPF_ITEM_NAI); /* connected address type. */
+        slice_set_uint16_le(output, 10, 0); /* No connection ID. */
+        slice_set_uint16_le(output, 12, CPF_ITEM_UDI); /* connected data type */
+        slice_set_uint16_le(output, 14, (uint16_t)slice_len(result)); /* result from CIP processing downstream. */
+
+        /* create a new slice with the CPF header and the response packet in it. */
+        result = slice_from_slice(output, (size_t)0, (size_t)(slice_len(result) + (ssize_t)CPF_UCONN_HEADER_SIZE));
+    }
+
+    /* errors are pass through. */
+
+    return result;
+
+}
+
+
+
+slice_s handle_cpf_connected(slice_s input, slice_s output, plc_s *plc)
+{
+    slice_s result;
+    cpf_co_header_s header;
+
+    /* we must have some sort of payload. */
+    if(slice_len(input) <= CPF_UCONN_HEADER_SIZE) {
+        info("Unusable size of connected CPF packet!");
+        return slice_make_err(EIP_ERR_BAD_REQUEST);
+    }
+
+    /* unpack the request. */
+    header.interface_handle = slice_get_uint32_le(input, 0);
+    header.router_timeout = slice_get_uint16_le(input, 4);
+    header.item_count = slice_get_uint16_le(input, 6);
+
+    /* sanity check the number of items. */
+    if(header.item_count != (uint16_t)2) {
+        info("Unsupported connected CPF packet, expected two items but found %u!", header.item_count);
+        return slice_make_err(EIP_ERR_BAD_REQUEST);
+    }
+
+    header.item_addr_type = slice_get_uint16_le(input, 8);
+    header.item_addr_length = slice_get_uint16_le(input, 10);
+    header.conn_id = slice_get_uint32_le(input, 12);
+    header.item_data_type = slice_get_uint16_le(input, 16);
+    header.item_data_length = slice_get_uint16_le(input, 18);
+    header.conn_seq = slice_get_uint16_le(input, 20);
+
+    /* sanity check the data. */
+    if(header.item_addr_type != CPF_ITEM_CAI) {
+        info("Expected connected address item but found %x!", header.item_addr_type);
+        return slice_make_err(EIP_ERR_BAD_REQUEST);
+    }
+
+    if(header.item_addr_length != 4) {
+        info("Expected address item length of 4 but found %d bytes!", header.item_addr_length);
+        return slice_make_err(EIP_ERR_BAD_REQUEST);
+    }
+
+    if(header.conn_id != plc->server_connection_id) {
+        info("Expected connection ID %x but found connection ID %x!", plc->server_connection_id, header.conn_id);
+        return slice_make_err(EIP_ERR_BAD_REQUEST);
+    }
+
+    if(header.item_data_type != CPF_ITEM_CDI) {
+        info("Expected connected data item but found %x!", header.item_data_type);
+        return slice_make_err(EIP_ERR_BAD_REQUEST);
+    }
+
+    if(header.item_data_length != (slice_len(input) - (CPF_CONN_HEADER_SIZE - 2))) {
+        info("CPF payload length, %d, does not match passed length, %d!", (slice_len(input) - (CPF_CONN_HEADER_SIZE - 2)), header.item_data_length);
+        return slice_make_err(EIP_ERR_BAD_REQUEST);
+    }
+
+    /* do we care about the sequence ID?   Should check. */
+    plc->server_connection_seq = header.conn_seq;
+
+    /* dispatch and handle the result. */
+    result = cip_dispatch_request(slice_from_slice(input,  (size_t)CPF_CONN_HEADER_SIZE, (size_t)((uint16_t)slice_len(input) - CPF_CONN_HEADER_SIZE)),
+                                  slice_from_slice(output, (size_t)CPF_CONN_HEADER_SIZE, (size_t)((uint16_t)slice_len(output) - CPF_CONN_HEADER_SIZE)),
+                                  plc);
+
+    if(!slice_has_err(result)) {
+        /* build outbound header. */
+        slice_set_uint16_le(output, 0, 2); /* two items. */
+        slice_set_uint16_le(output, 2, CPF_ITEM_CAI); /* connected address type. */
+        slice_set_uint16_le(output, 4, 4); /* connection ID is 4 bytes. */
+        slice_set_uint32_le(output, 6, plc->client_connection_id);
+        slice_set_uint16_le(output, 10, CPF_ITEM_CDI); /* connected data type */
+        slice_set_uint16_le(output, 12, (uint16_t)(slice_len(result) + 2)); /* result from CIP processing downstream.  Plus 2 bytes for sequence number. */
+        slice_set_uint16_le(output, 14, plc->client_connection_seq);
+
+        /* create a new slice with the CPF header and the response packet in it. */
+        result = slice_from_slice(output, (size_t)0, (size_t)(slice_len(result) + CPF_CONN_HEADER_SIZE));
+    }
+
+    /* errors are pass through. */
+
+    return result;
+}
+
---
+++ src/tests/ab_server/src/socket.c
@@ -0,0 +1,267 @@
+/***************************************************************************
+ *   Copyright (C) 2020 by Kyle Hayes                                      *
+ *   Author Kyle Hayes  kyle.hayes@gmail.com                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#if defined(WIN32) || defined(_WIN32) || defined(_WIN64)
+    #include <winsock2.h>
+    #include <ws2tcpip.h>
+#else
+    #include <arpa/inet.h>
+    #include <errno.h>
+    #include <netdb.h>
+    #include <netinet/in.h>
+    #include <sys/socket.h>
+    #include <sys/time.h>
+    #include <sys/types.h>
+    #include <unistd.h>
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "slice.h"
+#include "socket.h"
+#include "utils.h"
+
+
+#define LISTEN_QUEUE (10)
+
+int socket_open(const char *host, const char *port)
+{
+	//int status;
+	struct addrinfo addr_hints;
+    struct addrinfo *addr_info = NULL;
+	int sock;
+    int sock_opt = 0;
+    int rc;
+
+#ifdef _WIN32
+	/* Windows needs special initialization. */
+	WSADATA winsock_data;
+	rc = WSAStartup(MAKEWORD(2, 2), &winsock_data);
+
+	if (rc != NO_ERROR) {
+		info("WSAStartup failed with error: %d\n", rc);
+		return SOCKET_ERR_STARTUP;
+	}
+#endif
+
+    /*
+     * Set up the hints for the type of socket that we want.
+     */
+
+    /* make sure the whole struct is set to 0 bytes */
+	memset(&addr_hints, 0, sizeof addr_hints);
+
+    /*
+     * From the man page (node == host name here):
+     *
+     * "If the AI_PASSIVE flag is specified in hints.ai_flags, and node is NULL, then
+     * the returned socket addresses will be suitable for bind(2)ing a socket that
+     * will  accept(2) connections.   The returned  socket  address  will  contain
+     * the  "wildcard  address" (INADDR_ANY for IPv4 addresses, IN6ADDR_ANY_INIT for
+     * IPv6 address).  The wildcard address is used by applications (typically servers)
+     * that intend to accept connections on any of the host's network addresses.  If
+     * node is not NULL, then the AI_PASSIVE flag is ignored.
+     *
+     * If the AI_PASSIVE flag is not set in hints.ai_flags, then the returned socket
+     * addresses will be suitable for use with connect(2), sendto(2), or sendmsg(2).
+     * If node is NULL, then the  network address  will  be  set  to the loopback
+     * interface address (INADDR_LOOPBACK for IPv4 addresses, IN6ADDR_LOOPBACK_INIT for
+     * IPv6 address); this is used by applications that intend to communicate with
+     * peers running on the same host."
+     *
+     * So we can get away with just setting AI_PASSIVE.
+     */
+    addr_hints.ai_flags = AI_PASSIVE;
+
+    /* this allows for both IPv4 and IPv6 */
+    addr_hints.ai_family = AF_UNSPEC;
+
+    /* we want a TCP socket.  And we want it now! */
+    addr_hints.ai_socktype = SOCK_STREAM;
+
+    /* Get the address info about the local system, to be used later. */
+    rc = getaddrinfo(host, port, &addr_hints, &addr_info);
+    if (rc != 0) {
+        info("ERROR: getaddrinfo() failed: %s\n", gai_strerror(rc));
+        return SOCKET_ERR_OPEN;
+    }
+
+    /* finally, finally, finally, we get to open a socket! */
+    sock = socket(addr_info->ai_family, addr_info->ai_socktype, addr_info->ai_protocol);
+
+    if (sock < 0) {
+        info("ERROR: socket() failed: %s\n", gai_strerror(sock));
+        return SOCKET_ERR_CREATE;
+    }
+
+    /* if this is going to be a server socket, bind it. */
+    if(strcmp(host,"0.0.0.0") == 0) {
+        info("socket_open() setting up server socket.   Binding to address 0.0.0.0.");
+
+        rc = bind(sock, addr_info->ai_addr, addr_info->ai_addrlen);
+        if (rc < 0)	{
+            printf("ERROR: Unable to bind() socket: %s\n", gai_strerror(rc));
+            return SOCKET_ERR_BIND;
+        }
+
+        rc = listen(sock, LISTEN_QUEUE);
+        if(rc < 0) {
+            info("ERROR: Unable to call listen() on socket: %s\n", gai_strerror(rc));
+            return SOCKET_ERR_LISTEN;
+        }
+
+        /* set up our socket to allow reuse if we crash suddenly. */
+        sock_opt = 1;
+        rc = setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (char*)&sock_opt, sizeof(sock_opt));
+        if(rc) {
+            socket_close(sock);
+            info("ERROR: Setting SO_REUSEADDR on socket failed: %s\n", gai_strerror(rc));
+            return SOCKET_ERR_SETOPT;
+        }
+    } else {
+        struct timeval timeout; /* used for timing out connections etc. */
+        struct linger so_linger; /* used to set up short/no lingering after connections are close()ed. */
+
+#ifdef SO_NOSIGPIPE
+        /* On *BSD and macOS, set the socket option to prevent SIGPIPE. */
+        rc = setsockopt(sock, SOL_SOCKET, SO_NOSIGPIPE, (char*)&sock_opt, sizeof(sock_opt));
+        if(rc) {
+            socket_close(sock);
+            info ("ERROR: Setting SO_REUSEADDR on socket failed: %s\n", gai_strerror(rc));
+            return SOCKET_ERR_SETOPT;
+        }
+#endif
+
+        timeout.tv_sec = 10;
+        timeout.tv_usec = 0;
+
+        rc = setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(timeout));
+        if(rc) {
+            socket_close(sock);
+            info("ERROR: Setting SO_RCVTIMEO on socket failed: %s\n", gai_strerror(rc));
+            return SOCKET_ERR_SETOPT;
+        }
+
+        rc = setsockopt(sock, SOL_SOCKET, SO_SNDTIMEO, (char*)&timeout, sizeof(timeout));
+        if(rc) {
+            socket_close(sock);
+            info("ERROR: Setting SO_SNDTIMEO on socket failed: %s\n", gai_strerror(rc));
+            return SOCKET_ERR_SETOPT;
+        }
+
+        /* abort the connection on close. */
+        so_linger.l_onoff = 1;
+        so_linger.l_linger = 0;
+
+        rc = setsockopt(sock, SOL_SOCKET, SO_LINGER,(char*)&so_linger,sizeof(so_linger));
+        if(rc) {
+            socket_close(sock);
+            info("ERROR: Setting SO_LINGER on socket failed: %s\n", gai_strerror(rc));
+            return SOCKET_ERR_SETOPT;
+        }
+    }
+
+    /* free the memory for the address info struct. */
+    freeaddrinfo(addr_info);
+
+    return sock;
+}
+
+
+
+void socket_close(int sock)
+{
+    if(sock >= 0) {
+#ifdef WIN32
+        closesocket(sock);
+#else
+        close(sock);
+#endif
+    }
+}
+
+
+int socket_accept(int sock)
+{
+    return accept(sock, NULL, NULL);
+}
+
+
+slice_s socket_read(int sock, slice_s in_buf)
+{
+    int rc = (int)recv(sock, (char *)in_buf.data, (size_t)in_buf.len, 0);
+
+    if(rc < 0) {
+#ifdef WIN32
+        rc = WSAGetLastError();
+        if(rc == WSAEWOULDBLOCK) {
+#else
+        rc = errno;
+        if(rc == EAGAIN || rc == EWOULDBLOCK) {
+#endif
+            rc = 0;
+        } else {
+            info("Socket read error rc=%d.\n", rc);
+            rc = SOCKET_ERR_READ;
+        }
+    }
+
+    return ((rc>=0) ? slice_from_slice(in_buf, 0, (size_t)(unsigned int)rc) : slice_make_err(rc));
+}
+
+
+/* this blocks until all the data is written or there is an error. */
+int socket_write(int sock, slice_s out_buf)
+{
+    size_t total_bytes_written = 0;
+    int rc = 0;
+    slice_s tmp_out_buf = out_buf;
+
+    info("socket_write(): writing packet:");
+    slice_dump(out_buf);
+
+    do {
+        rc = (int)send(sock, (char *)tmp_out_buf.data, (size_t)tmp_out_buf.len, 0);
+
+        /* was there an error? */
+        if(rc < 0) {
+            /*
+             * check the return value.  If it is an interrupted system call
+             * or would block, just keep looping.
+             */
+#ifdef WIN32
+            rc = WSAGetLastError();
+            if(rc != WSAEWOULDBLOCK) {
+#else
+            rc = errno;
+            if(rc != EAGAIN && rc != EWOULDBLOCK) {
+#endif
+                info("Socket write error rc=%d.\n", rc);
+                return SOCKET_ERR_WRITE;
+            }
+        } else {
+            total_bytes_written += (size_t)rc;
+            tmp_out_buf = slice_from_slice(out_buf, total_bytes_written, slice_len(out_buf) - total_bytes_written);
+        }
+    } while(total_bytes_written < slice_len(out_buf));
+
+    return (int)(unsigned int)total_bytes_written;
+}
+
