cc480a246811da81263019a0b571735e6ba8abe6
Adam Dunsmoor <adamdunsmoor@pm.me>
2020-06-07 21:39:27 -0500

    update C++ contrib code with latest version of the library

--- src/wrappers/cpp/include/logging.hpp
+++ src/wrappers/cpp/include/logging.hpp
@@ -1,8 +1,3 @@
-/*
- * This file is subject to the terms and conditions defined in
- * file 'LICENSE.txt', which is part of this source code bundle.
- */
-
 #ifndef LOGGING_HPP
 #define LOGGING_HPP
 
@@ -12,12 +7,13 @@
 #include <boost/log/trivial.hpp>
 #include <boost/log/utility/setup.hpp>
 
-#define TRACE BOOST_LOG_SEV(my_logger::get(), boost::log::trivial::trace) << __FILE__ << "(" << __FUNCTION__ << ":" << __LINE__ << ") >> "
-#define DEBUG BOOST_LOG_SEV(my_logger::get(), boost::log::trivial::debug) << __FILE__ << "(" << __FUNCTION__ << ":" << __LINE__ << ") >> "
-#define INFO  BOOST_LOG_SEV(my_logger::get(), boost::log::trivial::info) << __FILE__ << "(" << __FUNCTION__ << ":" << __LINE__ << ") >> "
-#define WARNING  BOOST_LOG_SEV(my_logger::get(), boost::log::trivial::warning) << __FILE__ << "(" << __FUNCTION__ << ":" << __LINE__ << ") >> "
-#define ERROR BOOST_LOG_SEV(my_logger::get(), boost::log::trivial::error) << __FILE__ << "(" << __FUNCTION__ << ":" << __LINE__ << ") >> "
-#define FATAL BOOST_LOG_SEV(my_logger::get(), boost::log::trivial::fatal) << __FILE__ << "(" << __FUNCTION__ << ":" << __LINE__ << ") >> "
+#define __FILENAME__ (__builtin_strrchr(__FILE__, '/') ? __builtin_strrchr(__FILE__, '/') + 1 : __FILE__)
+#define TRACE BOOST_LOG_SEV(my_logger::get(), boost::log::trivial::trace) << __FILENAME__ << "/" << __FUNCTION__ << ":" << __LINE__ << " >> "
+#define DEBUG BOOST_LOG_SEV(my_logger::get(), boost::log::trivial::debug) << __FILENAME__ << "/" << __FUNCTION__ << ":" << __LINE__ << " >> "
+#define INFO BOOST_LOG_SEV(my_logger::get(), boost::log::trivial::info) << __FILENAME__ << "/" << __FUNCTION__ << ":" << __LINE__ << " >> "
+#define WARNING BOOST_LOG_SEV(my_logger::get(), boost::log::trivial::warning) << __FILENAME__ << "/" << __FUNCTION__ << ":" << __LINE__ << " >> "
+#define ERROR BOOST_LOG_SEV(my_logger::get(), boost::log::trivial::error) << __FILENAME__ << "/" << __FUNCTION__ << ":" << __LINE__ << " >> "
+#define FATAL BOOST_LOG_SEV(my_logger::get(), boost::log::trivial::fatal) << __FILENAME__ << "/" << __FUNCTION__ << ":" << __LINE__ << " >> "
 
 #define SYS_LOGFILE "logs/main_%y-%m-%d_#%N.log"
 
