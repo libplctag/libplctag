91e161c73e30a541e297c2e28c9c27da119933a1
Kyle Hayes <kyle.hayes@gmail.com>
2020-06-21 07:13:29 -0700

    Added initial hooks for Modbus.   Refactored lib init code to add mutex.

--- src/protocols/mb/modbus.c
+++ src/protocols/mb/modbus.c
@@ -31,17 +31,28 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-#ifndef __LIBPLCTAG_AB_H__
-#define __LIBPLCTAG_AB_H__ 1
+
+#include <mb/modbus.h>
+
+void mb_teardown(void)
+{
+    /* place holder */
+}
 
 
-#include <lib/libplctag.h>
-#include <lib/tag.h>
-#include <util/attr.h>
 
-void ab_teardown(void);
-int ab_init();
-plc_tag_p ab_tag_create(attr attribs);
+int mb_init()
+{
+    /* place holder */
 
+    return PLCTAG_STATUS_OK;
+}
 
-#endif
+plc_tag_p mb_tag_create(attr attribs)
+{
+    (void)attribs;
+
+    /* place holder */
+
+    return NULL;
+}
