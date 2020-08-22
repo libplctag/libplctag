87ecaf9421fa3662b83499ed6d6aba00a1447bbc
Kyle Hayes <kyle.hayes@gmail.com>
2020-08-22 13:39:01 -0700

    Tweaked time_ms() and fixed off-by-one for the month on date output in logs.

--- src/platform/windows/platform.c
+++ src/platform/windows/platform.c
@@ -1598,18 +1598,18 @@ int sleep_ms(int ms)
 
 int64_t time_ms(void)
 {
-    /* Found on StackOverflow in an answer by Ian Boyd.  Triggered by user @xieqiang. */
-    int64_t res = 0;
-    const int64_t UNIX_TIME_START = 0x019DB1DED53E8000;   /* January 1, 1970 (start of Unix epoch) in "ticks" */
-    const int64_t TICKS_PER_MILLISECOND = 10000;          /* a tick is 100ns */
     FILETIME ft;
+    int64_t res;
 
-    GetSystemTimeAsFileTime(&ft); //returns ticks in UTC
+    GetSystemTimeAsFileTime(&ft);
 
+    /* calculate time as 100ns increments since Jan 1, 1601. */
     res = (int64_t)(ft.dwLowDateTime) + ((int64_t)(ft.dwHighDateTime) << 32);
 
-    /* Convert ticks since 1/1/1970 into milliseconds */
-    return (res - UNIX_TIME_START) / TICKS_PER_MILLISECOND;    
+    /* get time in ms.   Magic offset is for Jan 1, 1970 Unix epoch baseline. */
+    res = (res - 116444736000000000) / 10000;
+
+    return  res;
 }
 
 
