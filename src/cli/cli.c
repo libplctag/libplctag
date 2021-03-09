0c123798dfaaaf1a39daebe195677b5397f8e881
Atif Ali <atifshoukatali@yahoo.com>
2021-03-09 12:36:30 -0800

    include basic cli stub

---
+++ src/cli/cli.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../lib/libplctag.h"
+
+void setup()
+{
+    printf("Hello from the CLI!\n");
+}
+
+int main(int argc, char **argv)
+{
+    setup();
+    return 0;
+}
---
+++ src/cli/cli.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <platform.h>
+
+void setup(void);
