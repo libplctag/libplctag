df518057e283047a9874fdd8c58d14ec24416643
Atif Ali <atifshoukatali@yahoo.com>
2021-03-09 16:23:08 -0800

    include basic cli arg parsing stub

--- src/cli/cli.c
+++ src/cli/cli.c
@@ -1,14 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../lib/libplctag.h"
+#include "./cli.h"
 
-void setup()
+void parse_args(int argc, char *argv[], request_t *request)
 {
-    printf("Hello from the CLI!\n");
+    if (argc != 3) {
+        printf("ERROR: invalid number of arguments!\n");
+        return;
+    }
+
+    char *operation = argv[1];
+    char *args = argv[2];
+
+    if (!strcmp(operation, "--read")) {
+        printf("cli operation: READ.\n");
+        request->operation = CLI_READ_OPERATION;
+    } else if (!strcmp(operation, "--watch")) {
+        printf("cli operation: WATCH.\n");
+        request->operation = CLI_WATCH_OPERATION;
+    } else if (!strcmp(operation, "--write")) {
+        printf("cli operation: WRITE.\n");
+        request->operation = CLI_WRITE_OPERATION;
+    } else {
+        printf("cli operation: INVALID.\n");
+        return;
+    }
+
+    char *args_l = (char *) malloc(strlen(args));
+    strcpy(args_l, args);
+    
+    printf("cli %s: ", strtok(args_l, "="));
+    request->ip = strtok(NULL, "=");
+    printf("%s\n", request->ip);
+    
+    free(args_l);
+    return;    
 }
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    setup();
+    request_t request = Request;
+    parse_args(argc, argv, &request);
+
+    if (request.operation == CLI_INVALID_OPERATION) {
+        printf("ERROR: operation invalid!\n");
+        exit(-1);
+    }
+
+    if (request.ip == NULL) {
+        printf("ERROR: ip invalid!\n");
+        exit(-1);
+    }
+
+    switch (request.operation) {
+        case CLI_READ_OPERATION:
+            printf("Read here!\n");
+            break;
+        case CLI_WATCH_OPERATION:
+            printf("Watch here!\n");
+            break;
+        case CLI_WRITE_OPERATION:
+            printf("Write here!\n");
+            break;
+        default:
+            break;
+    }
+
     return 0;
 }
--- src/cli/cli.h
+++ src/cli/cli.h
@@ -2,4 +2,17 @@
 
 #include <platform.h>
 
-void setup(void);
+/* cli operations */
+#define CLI_INVALID_OPERATION      (-1)
+#define CLI_READ_OPERATION          (0)
+#define CLI_WATCH_OPERATION         (1)
+#define CLI_WRITE_OPERATION         (2)
+
+struct request_s {
+    int operation;
+    char *ip;
+} Request = {-1, NULL};
+
+typedef struct request_s request_t;
+
+void parse_args(int argc, char *argv[], request_t *request);
