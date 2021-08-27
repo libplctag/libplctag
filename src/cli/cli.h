dbe22782a774548500d7b2f3c3db3851b3e32cd5
Atif Shoukat Ali <atifshoukatali@yahoo.com>
2021-08-26 20:31:23 -0700

    add support for a basic CLI interface (#267)
    * include basic cli stub
    
    * include basic cli arg parsing stub
    
    * dirty POC ugghh
    
    * change conflicting function names
    
    * include getline for windows
    
    * replace bool accessors
    
    * include tag info on get
    
    * don't use pointer for tag create
    
    * include mem allocation for tag attrs
    
    * create new tag struct every loop
    
    * add watch interval cli option - POC DONE 🎉
    
    * refactor read tags
    
    * switch strcasecmp with strcmp
    
    * refactor write tags
    
    * refactor watch tags
    
    * use write_val for set tags
    
    * donot quit when watching tags
    
    * include debugging for write verify
    
    * print bool val when verifying write
    
    * include obasic offline mode
    
    * include sleeps in while loops
    
    * remove redundant stuff from cmake
    
    * remove util dependency
    
    * revert cmake file
    
    * remove utils header
    
    * try utils with windows now
    
    * bool debugging prompts
    
    * include break for getline
    
    * include bool debug after tag get
    
    * add flush
    
    * include tag name in write failure
    
    * remove break logic
    
    * print debug info bool write fail
    
    * change get tag to pointers
    
    * change get tag to pointers
    
    * remove debug info
    
    * include tag pointer for sets
    
    * include bit offset parsing
    
    * assign type bool if bit offset set
    
    * use bit accessor for bit tags
    
    * destroy tags before shutdown
    
    * return rc on verify tags
    
    * change getline to header
    
    * try exit instead of return
    
    * try exit 1
    
    * make all returns exits in main
    
    * replace = with : in json 🤦
    
    * include cli param for additional attributes
    
    * make offline flag a bool
    
    * use bool print method
    
    * change tag line format
    
    * use offset from tag in get/set
    
    * include tag line validation
    
    * ignore trailing = for params
    
    * copy tag key and path
    
    * add more tag processing logs
    
    * include more logging fot tag processing
    
    * include log for part processing
    
    * refactor tag line parts
    
    * use libplactag lfor logging
    
    * disable secure warnings
    
    * try compiler flags instead
    
    * explicitly add debug files
    
    * set util libs explicitly
    
    * include debug header
    
    * try adding debug src file
    
    * include lib srcs
    
    * link cli dynamically to libplctag
    
    * try adding lib srcs directly
    
    * flush std streams after output
    
    * update usage output
    
    * remove unnecessary lib version check
    
    * add README
    
    * include debugging while waiting for tags
    
    * increase wait time for tag checking
    
    * print status of tag not ok
    
    * include function call
    
    * pass status int not handle
    
    * increase tag wait time
    
    * revert sleep time back while waiting
    
    * set default params for modbus
    
    * check tag status before read
    
    * remove redundant check

---
+++ src/cli/cli.h
@@ -0,0 +1,99 @@
+#pragma once
+
+#include "./uthash.h"
+
+/* tag paths */
+#define TAG_PATH                  "protocol=%s&gateway=%s&path=%s&plc=%s&debug=%d&name=%s%s"
+#define TAG_PATH_AUTO_READ_SYNC   "protocol=%s&gateway=%s&path=%s&plc=%s&debug=%d&auto_sync_read_ms=%d&name=%s%s"
+#define DATA_TIMEOUT              5000
+
+/* bool utils */
+#define btoa(x) ( (x) ? "true" : "false" )
+
+/* cli operations */
+typedef enum {
+    READ,
+    WRITE,
+    WATCH
+} cli_operation_t;
+
+/* cli request definition */
+typedef struct {
+    const char *protocol;
+    const char *ip;
+    const char *path;
+    const char *plc;
+    cli_operation_t operation;
+    int interval;
+    int debug_level;
+    const char *attributes;
+    bool offline;
+} cli_request_t;
+
+/* tag line parts definition */
+typedef struct {
+    char **parts;
+    int num_parts;
+} tag_line_parts_t;
+
+/* data types */
+typedef enum {
+    t_UINT64, t_INT64,
+    t_UINT32, t_INT32,
+    t_UINT16, t_INT16,
+    t_UINT8, t_INT8, 
+    t_FLOAT64, t_FLOAT32, 
+    t_BOOL 
+} data_type_t;
+
+/* tag definition */
+typedef struct {
+    const char *key;
+    data_type_t type;
+    const char *path;
+    union {
+        uint64_t UINT64_val;
+        int64_t INT64_val;
+        uint32_t UINT32_val;
+        int32_t INT32_val;
+        uint16_t UINT16_val;
+        int16_t INT16_val;
+        uint8_t UINT8_val;
+        int8_t INT8_val;
+        double FLOAT64_val;
+        float FLOAT32_val;
+        bool BOOL_val;
+    } val, last_val, write_val;
+    int bit;
+    int offset;
+    bool watch;
+} tag_t;
+
+/* tags hash definition */
+struct tags {
+    int tag_handle;
+    tag_t tag;
+    UT_hash_handle hh;
+};
+
+void usage(void);
+int parse_args(int argc, char *argv[]);
+void print_request();
+int process_tags();
+int is_comment(const char *line);
+void trim_line(char *line);
+tag_line_parts_t split_string(const char *str, const char *sep);
+int process_line(const char *line, tag_t *tag);
+int validate_line(tag_line_parts_t tag_line_parts);
+void print_tag(tag_t *tag);
+void add_tag(int32_t tag_handle, tag_t tag);
+int check_tags(void);
+int get_tag(int32_t tag_handle, tag_t *tag);
+int read_tags(void);
+int set_tag(int32_t tag_handle, tag_t *tag);
+int verify_write_tags(void);
+int write_tags(void);
+int watch_tags(void);
+void tag_callback(int32_t tag_handle, int event, int status);
+int destroy_tags(void);
+int do_offline(void);
