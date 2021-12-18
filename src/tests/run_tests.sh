3db71d88f8fbdbcaae76f1a83dcd9e2477f22cf6
Kyle Hayes <kyle.hayes@gmail.com>
2021-12-18 13:41:21 -0800

    Add more tests and test renaming.

--- src/tests/run_tests.sh
+++ src/tests/run_tests.sh
@@ -17,7 +17,7 @@ if [[ ! -d $TEST_DIR ]]; then
 fi
 
 # test for the executables.
-EXECUTABLES="ab_server tag_rw2 list_tags thread_stress test_callback"
+EXECUTABLES="ab_server tag_rw2 list_tags test_auto_sync test_callback test_raw_cip test_reconnect thread_stress"
 # echo -n "  Checking for executables..."
 for EXECUTABLE in $EXECUTABLES
 do
@@ -32,7 +32,7 @@ done
 
 let TEST++
 echo -n "Test $TEST: basic large tag read/write... "
-$TEST_DIR/tag_rw2 --type=sint32 '--tag=protocol=ab-eip&gateway=10.206.1.40&path=1,4&plc=ControlLogix&elem_count=1000&name=TestBigArray' --debug=4 --write=1,2,3,4,5,6,7,8,9 > big_tag_test.log 2>&1
+$TEST_DIR/tag_rw2 --type=sint32 '--tag=protocol=ab-eip&gateway=10.206.1.40&path=1,4&plc=ControlLogix&elem_count=1000&name=TestBigArray' --debug=4 --write=1,2,3,4,5,6,7,8,9 > "${TEST}_big_tag_test.log" 2>&1
 if [ $? != 0 ]; then
     echo "FAILURE"
     let FAILURES++
@@ -43,7 +43,7 @@ fi
 
 let TEST++
 echo -n "Test $TEST: basic Micrologix tag read/write... "
-$TEST_DIR/tag_rw2 --type=uint8 '--tag=protocol=ab-eip&gateway=10.206.1.36&plc=micrologix&elem_count=1&elem_size=2&name=N7:0/14' --write=0 --debug=4 > micrologix.log 2>&1
+$TEST_DIR/tag_rw2 --type=uint8 '--tag=protocol=ab-eip&gateway=10.206.1.36&plc=micrologix&elem_count=1&elem_size=2&name=N7:0/14' --write=0 --debug=4 > "${TEST}_micrologix.log" 2>&1
 if [ $? != 0 ]; then
     echo "FAILURE"
     let FAILURES++
@@ -54,7 +54,7 @@ fi
 
 let TEST++
 echo -n "Test $TEST: basic PLC-5 tag read/write... "
-$TEST_DIR/tag_rw2 --type=uint8 '--tag=protocol=ab-eip&gateway=10.206.1.38&plc=plc5&elem_count=1&elem_size=2&name=B3:0/10' --debug=4 --write=0 > plc5.log 2>&1
+$TEST_DIR/tag_rw2 --type=uint8 '--tag=protocol=ab-eip&gateway=10.206.1.38&plc=plc5&elem_count=1&elem_size=2&name=B3:0/10' --debug=4 --write=0 > "${TEST}_plc5.log" 2>&1
 if [ $? != 0 ]; then
     echo "FAILURE"
     let FAILURES++
@@ -66,7 +66,7 @@ fi
 
 let TEST++
 echo -n "Test $TEST: basic DH+ bridging... "
-$TEST_DIR/tag_rw2 --type=uint8 '--tag=protocol=ab_eip&gateway=10.206.1.40&path=1,2,A:27:1&cpu=plc5&elem_count=1&elem_size=2&name=B3:0/10' --debug=4 --write=0  > dhp_bridge.log 2>&1
+$TEST_DIR/tag_rw2 --type=uint8 '--tag=protocol=ab_eip&gateway=10.206.1.40&path=1,2,A:27:1&cpu=plc5&elem_count=1&elem_size=2&name=B3:0/10' --debug=4 --write=0  > "${TEST}_dhp_bridge.log" 2>&1
 if [ $? != 0 ]; then
     echo "FAILURE"
     let FAILURES++
@@ -78,7 +78,19 @@ fi
 
 let TEST++
 echo -n "Test $TEST: basic CIP bridging... "
-$TEST_DIR//tag_rw2 --type=sint32 '--tag=protocol=ab_eip&gateway=10.206.1.39&path=1,6,18,10.206.1.40,1,4&plc=lgx&name=TestBigArray[0]' --debug=4 --write=5 > cip_bridge.log 2>&1
+$TEST_DIR//tag_rw2 --type=sint32 '--tag=protocol=ab_eip&gateway=10.206.1.39&path=1,6,18,10.206.1.40,1,4&plc=lgx&name=TestBigArray[0]' --debug=4 --write=5 > "${TEST}_cip_bridge.log" 2>&1
+if [ $? != 0 ]; then
+    echo "FAILURE"
+    let FAILURES++
+else
+    echo "OK"
+    let SUCCESSES++
+fi
+
+
+let TEST++
+echo -n "Test $TEST: raw cip tag... "
+$TEST_DIR/test_raw_cip > "${TEST}_raw_cip_test.log" 2>&1
 if [ $? != 0 ]; then
     echo "FAILURE"
     let FAILURES++
@@ -90,7 +102,7 @@ fi
 
 let TEST++
 echo -n "Test $TEST: tag listing... "
-$TEST_DIR/list_tags "10.206.1.40" "1,4" > list_tags_test.log 2>&1
+$TEST_DIR/list_tags "10.206.1.40" "1,4" > "${TEST}_list_tags_test.log" 2>&1
 if [ $? != 0 ]; then
     echo "FAILURE"
     let FAILURES++
@@ -102,7 +114,18 @@ fi
 
 let TEST++
 echo -n "Test $TEST: CIP thread stress... "
-$TEST_DIR/thread_stress 20 "protocol=ab-eip&gateway=10.206.1.40&path=1,4&plc=ControlLogix&name=TestBigArray" > thread_stress_test.log 2>&1
+$TEST_DIR/thread_stress 20 "protocol=ab-eip&gateway=10.206.1.40&path=1,4&plc=ControlLogix&name=TestBigArray" > "${TEST}_thread_stress_test.log" 2>&1
+if [ $? != 0 ]; then
+    echo "FAILURE"
+    let FAILURES++
+else
+    echo "OK"
+    let SUCCESSES++
+fi
+
+let TEST++
+echo -n "Test $TEST: auto sync... "
+$TEST_DIR/test_auto_sync > "${TEST}_auto_sync_test.log" 2>&1
 if [ $? != 0 ]; then
     echo "FAILURE"
     let FAILURES++
@@ -128,7 +151,7 @@ fi
 
 let TEST++
 echo -n "Test $TEST: emulator test callbacks... "
-$TEST_DIR/test_callback > callback_test.log 2>&1
+$TEST_DIR/test_callback > "${TEST}_callback_test.log" 2>&1
 if [ $? != 0 ]; then
     echo "FAILURE"
     let FAILURES++
@@ -156,7 +179,7 @@ fi
 
 let TEST++
 echo -n "Test $TEST: basic Omron read/write... "
-$TEST_DIR/./tag_rw2 --type=sint32  '--tag=protocol=ab-eip&gateway=127.0.0.1&path=18,127.0.0.1&plc=omron-njnx&name=TestDINTArray' --write=42 --debug=4 > omron_tag_test.log 2>&1
+$TEST_DIR/./tag_rw2 --type=sint32  '--tag=protocol=ab-eip&gateway=127.0.0.1&path=18,127.0.0.1&plc=omron-njnx&name=TestDINTArray' --write=42 --debug=4 > "${TEST}_omron_tag_test.log" 2>&1
 if [ $? != 0 ]; then
     echo "FAILURE"
     let FAILURES++
@@ -184,8 +207,33 @@ else
 fi
 
 let TEST++
+echo -n "Test $TEST: test short reconnect with Modbus... "
+$TEST_DIR/test_reconnect 3 > "${TEST}_modbus_reconnect_short_test.log" 2>&1
+if [ $? != 0 ]; then
+    echo "FAILURE"
+    let FAILURES++
+else
+    echo "OK"
+    let SUCCESSES++
+fi
+
+
+let TEST++
+echo -n "Test $TEST: test long reconnect with Modbus... "
+$TEST_DIR/test_reconnect 10 > "${TEST}_modbus_reconnect_long_test.log" 2>&1
+if [ $? != 0 ]; then
+    echo "FAILURE"
+    let FAILURES++
+else
+    echo "OK"
+    let SUCCESSES++
+fi
+
+
+
+let TEST++
 echo -n "Test $TEST: thread stress Modbus... "
-$TEST_DIR/thread_stress 10 'protocol=modbus-tcp&gateway=127.0.0.1:5020&path=0&elem_count=2&name=hr10' > modbus_test.log 2>&1
+$TEST_DIR/thread_stress 10 'protocol=modbus-tcp&gateway=127.0.0.1:5020&path=0&elem_count=2&name=hr10' > "${TEST}_modbus_stress_test.log" 2>&1
 if [ $? != 0 ]; then
     echo "FAILURE"
     let FAILURES++
