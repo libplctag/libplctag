46d6e552d89cd3dd5daf65c448172bc5211eb49a
Kyle Hayes <kyle.hayes@gmail.com>
2024-06-23 08:34:52 -0700

    Had incorrect calculation of the instance entry length.  Was forgetting to count the name string.

--- src/examples/list_tags_omron.c
+++ src/examples/list_tags_omron.c
@@ -474,6 +474,8 @@ int32_t process_single_instance_data(int32_t tag, tag_entry_p tag_entry, uint32_
 
         /* calculate where we should end, 6 = 2 for length, 4 for ID */
         end_cursor = start_cursor + 6 + instance_data_len;
+        printf("INFO: start cursor %"PRIu32", cursor %"PRIu32", instance data length  %"PRIu16", end cursor %"PRIu32"\n",
+                start_cursor, cursor, instance_data_len, cursor);
 
         /* skip the class? */
         cursor += 2;
@@ -498,8 +500,8 @@ int32_t process_single_instance_data(int32_t tag, tag_entry_p tag_entry, uint32_
         printf("INFO: Processed instance %"PRIu32" with name %s starting at location %"PRIu32" and ending at location %"PRIu32".\n",
                 tag_entry->instance_id, tag_entry->tag_name, start_cursor, cursor);
 
-        /* bump the cursor past to the next one but take into account the padding */
-        cursor += name_length & 0x01;
+        /* bump the cursor past to the next entry but take into account the padding */
+        cursor += name_length + name_length & 0x01;
 
         if(cursor != end_cursor) {
             printf("ERROR: check for cursor position failed!  Expected %"PRIu32" but got %"PRIu32".\n", end_cursor, cursor);
