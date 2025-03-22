5f31464b7d0ca348a6b9b6584a4748ee9a0df1ad
Kyle Hayes <kyle.hayes@gmail.com>
2025-03-21 21:41:09 -0700

    Added random utilities for  better random output.

---
+++ src/util/random_utils.h
@@ -0,0 +1,49 @@
+/***************************************************************************
+ *   Copyright (C) 2025 by Kyle Hayes                                      *
+ *   Author Kyle Hayes  kyle.hayes@gmail.com                               *
+ *                                                                         *
+ * This software is available under either the Mozilla Public License      *
+ * version 2.0 or the GNU LGPL version 2 (or later) license, whichever     *
+ * you choose.                                                             *
+ *                                                                         *
+ * MPL 2.0:                                                                *
+ *                                                                         *
+ *   This Source Code Form is subject to the terms of the Mozilla Public   *
+ *   License, v. 2.0. If a copy of the MPL was not distributed with this   *
+ *   file, You can obtain one at http://mozilla.org/MPL/2.0/.              *
+ *                                                                         *
+ *                                                                         *
+ * LGPL 2:                                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#pragma once
+
+#include <stdint.h>
+
+#define RANDOM_U64_ERROR (UINT64_MAX)
+
+/**
+ * @brief Generates a random number between 0 (inclusive) and upper_bound (exclusive).
+ *
+ * The generation uses a relatively good (crypto secure?) platform-dependent function
+ * to make the random number.
+ *
+ * @param upper_bound The upper bound (exclusive) of the random number.
+ * @return uint64_t A random number in the specified range.
+ */
+extern uint64_t random_u64(uint64_t upper_bound);
