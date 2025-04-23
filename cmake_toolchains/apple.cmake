259bf35398229fc64f68e7396b6566f2441e3e74
wegel <simon@wegel.ca>
2025-04-22 20:04:15 -0400

    Fix cmake build when using eg FetchContent (#527)

--- cmake_toolchains/apple.cmake
+++ cmake_toolchains/apple.cmake
@@ -1,7 +1,7 @@
 
 set(CMAKE_MACOSX_RPATH ON)
 
-set(PLATFORM_SHIM_PATH "${CMAKE_SOURCE_DIR}/src/platform/posix" )
+set(PLATFORM_SHIM_PATH "${CMAKE_CURRENT_LIST_DIR}/../src/platform/posix" )
 
 set (EXTRA_LINKER_LIBS "${EXTRA_LINKER_LIBS}" pthread)
 
