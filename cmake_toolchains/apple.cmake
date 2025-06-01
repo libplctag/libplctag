b4afa1087eb52f4143471763800723cd79cb9e7a
Kyle Hayes <kyle.hayes@gmail.com>
2025-06-01 09:27:44 -0700

    Remove commented code.

--- cmake_toolchains/apple.cmake
+++ cmake_toolchains/apple.cmake
@@ -6,9 +6,6 @@ set(PLATFORM_SHIM_PATH "${CMAKE_CURRENT_LIST_DIR}/../src/platform/posix" )
 
 set (EXTRA_LINKER_LIBS "${EXTRA_LINKER_LIBS}" pthread)
 
-# set(STATIC_C_LINKER_OPTIONS "-static-libgcc")
-# set(STATIC_CXX_LINKER_OPTIONS "-static-libstdc++")
-
 message("EXTRA_LINKER_LIBS = ${EXTRA_LINKER_LIBS}")
 
 set(POSIX True)
