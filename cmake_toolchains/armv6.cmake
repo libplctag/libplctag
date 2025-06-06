09f1f3cb0141dfd8159eae8b863b71cc67ecb664
Kyle Hayes <kyle.hayes@gmail.com>
2025-06-06 09:46:47 -0700

    Fix #533 arm builds (#538)
    * Change runner image to Ubuntu 22.04 to ensure older glibc version for Debian compatibility.
    
    * Changes to build Linux Arm binaries more cleanly (and perhaps correctly!).

---
+++ cmake_toolchains/armv6.cmake
@@ -0,0 +1,12 @@
+message("Building for Linux (Ubuntu/Debian) Arm v6")
+
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_C_COMPILER arm-linux-gnueabi-gcc)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabi-g++)
+set(CMAKE_C_FLAGS "-march=armv6 -mfpu=vfp -mfloat-abi=softfp")
+set(CMAKE_CXX_FLAGS "-march=armv6 -mfpu=vfp -mfloat-abi=softfp")
+set(EXTRA_LINKER_LIBS "${EXTRA_LINKER_LIBS}" atomic)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
