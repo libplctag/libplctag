a996412c4bd9bd492272ed29a92d8db2dc7bf0f2
Kyle Hayes <kyle.hayes@gmail.com>
2025-11-30 07:33:50 -0800

    Out of bounds is not a WARN.  It is OK most of the time, for instance removing the element at 0 to use the vector as a queue.

--- src/utils/vector.c
+++ src/utils/vector.c
@@ -192,7 +192,7 @@ void *vector_get(vector_p vec, int index) {
     }
 
     if(index < 0 || index >= vec->len) {
-        pdebug(DEBUG_MODULE_UTILS, DEBUG_WARN, "Index is out of bounds!");
+        pdebug(DEBUG_MODULE_UTILS, DEBUG_DETAIL, "Index %d is out of bounds", index);
         return NULL;
     }
 
