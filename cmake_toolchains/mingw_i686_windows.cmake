486bc2f5ec086e839d407cdac21328f41b8405d5
Kyle Hayes <kyle.hayes@gmail.com>
2026-05-15 09:38:36 -0700

    fiddling with shells again.  Make the compiler name explicit so that we get the right one.

--- cmake_toolchains/mingw_i686_windows.cmake
+++ cmake_toolchains/mingw_i686_windows.cmake
@@ -12,8 +12,9 @@
 set(CMAKE_SYSTEM_NAME    Windows)
 set(CMAKE_SYSTEM_PROCESSOR x86)
 
-set(CMAKE_C_COMPILER gcc)
-set(CMAKE_CXX_COMPILER g++)
+# Prefer explicit triplet compilers to avoid any accidental host-toolchain pickup.
+set(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
 
 # Tell CMake where to find 32-bit headers and libraries.
 if(DEFINED ENV{MINGW_PREFIX})
