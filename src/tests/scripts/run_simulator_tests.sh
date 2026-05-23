4439c92fde3e639f56453a76a46bee8b388653d8
Kyle Hayes <kyle.hayes@gmail.com>
2026-05-22 18:14:22 -0700

    Add test to catch Omron hang bug.

--- src/tests/scripts/run_simulator_tests.sh
+++ src/tests/scripts/run_simulator_tests.sh
@@ -91,7 +91,7 @@ if [[ ! -d $TEST_DIR ]]; then
 fi
 
 # test for the executables.
-EXECUTABLES="ab_server modbus_server list_tags_logix string_non_standard_udt string_standard tag_rw2 test_connection_stress test_create_from_tag test_connection_tag test_connection_tag_late_join test_fairness test_auto_sync test_callback test_callback_ex test_callback_ex_logix test_callback_ex_modbus test_idle_disconnect test_modbus_multiple test_raw_cip test_reconnect_after_outage_async test_reconnect_after_outage_sync test_shutdown_cip test_shutdown_modbus test_shutdown_restart test_special test_string test_tag_attributes test_tag_type_attribute thread_stress"
+EXECUTABLES="ab_server modbus_server list_tags_logix string_non_standard_udt string_standard tag_rw2 test_connection_stress test_create_from_tag test_connection_tag test_connection_tag_late_join test_fairness test_auto_sync test_callback test_callback_ex test_callback_ex_logix test_callback_ex_modbus test_idle_disconnect test_modbus_multiple test_omron_destroy test_raw_cip test_reconnect_after_outage_async test_reconnect_after_outage_sync test_shutdown_cip test_shutdown_modbus test_shutdown_restart test_special test_string test_tag_attributes test_tag_type_attribute thread_stress"
 # echo -n "  Checking for executables..."
 for EXECUTABLE in $EXECUTABLES
 do
@@ -631,6 +631,17 @@ $VALGRIND$TEST_DIR/test_create_from_tag \
     "--clone-attrib=name=TestDINTArray[1]&elem_count=1" \
     "--connection-tag=protocol=ab-eip&gateway=127.0.0.1&path=18,127.0.0.1&plc=omron-njnx&name=@connection" \
     --timeout=10000 > "$LOG_DIR/${TEST}_create_from_tag_omron_test.log" 2>&1
+if [ $? != 0 ]; then
+    echo "FAILURE"
+    let FAILURES++
+else
+    echo "OK"
+    let SUCCESSES++
+fi
+
+let TEST++
+echo -n "  Test $TEST: plc_tag_destroy does not hang after Omron connection loss (issue #625)... "
+$VALGRIND$TEST_DIR/test_omron_destroy "${TEST_DIR}/ab_server" > "$LOG_DIR/${TEST}_omron_destroy_test.log" 2>&1
 if [ $? != 0 ]; then
     echo "FAILURE"
     let FAILURES++
