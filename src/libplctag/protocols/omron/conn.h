558e87084034f10a9b94f8f066456c68e838625a
Aleksandar Fabijanic <aleks-f@users.noreply.github.com>
2026-06-18 15:11:04 +0200

    635 terminating abort request race (#643)
    Fixes for issue #635
    
    ---------

--- src/libplctag/protocols/omron/conn.h
+++ src/libplctag/protocols/omron/conn.h
@@ -113,7 +113,7 @@ struct omron_conn_t {
     uint64_t packet_count;
 
     thread_p handler_thread;
-    volatile int terminating;
+    atomic_int32_t terminating;
     mutex_p mutex;
     cond_p wait_cond;
 
@@ -141,7 +141,7 @@ struct omron_request_t {
 
     /* flags for communicating with background thread */
     int resp_received;
-    int abort_request;
+    atomic_int32_t abort_request;
 
     /* debugging info */
     int tag_id;
